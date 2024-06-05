b9bef34c1b6ff635a4a8f2a3bc7568629647b111
Rouslan Lakhno <rouslanlakhno@gmail.com>
2024-06-05 14:46:27 -0400

    Exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,17 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# Ask the user for a store name
+print "Enter store name: "
+store_name = gets.chomp
+
+# Attempt to create a store with the inputted name
+store = Store.new(name: store_name)
+
+# Display error messages if save fails
+unless store.save
+  puts "Errors:"
+  store.errors.full_messages.each do |message|
+    puts message
+  end
+end
