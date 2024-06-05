6de482961897ff819b696453c5ce61024bb0243f
Rouslan Lakhno <rouslanlakhno@gmail.com>
2024-06-05 14:32:39 -0400

    Exercise 2

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,11 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+# Load the third store from the database
+@store3 = Store.find(3)
+
+# Delete the third store from the database
+@store3.destroy
+
+# Output the number of stores
+puts "Number of stores: #{Store.count}"
